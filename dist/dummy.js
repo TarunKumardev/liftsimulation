2bdf266711a77a466c1fa944050f8a30f95f9c0c
tarunkumarf22labs <119585731+tarunkumarf22labs@users.noreply.github.com>
2023-07-12 09:10:59 +0530

    @fix:fixed some ui changes

---
+++ dist/dummy.js
@@ -0,0 +1,18 @@
+// let data = 2 ;
+// let vdata
+// vdata = data + data
+
+// console.log(vdata);
+
+// function handledebugging(props){
+//     let sahi = props + 1 ;
+//     debugger
+//     // handledebuggingdata(sahi)
+//     console.log(sahi);
+// }
+
+// // function handledebuggingdata(props) {
+// //     bini =  props + 12;
+// //   debugger
+// // }
+// handledebugging(vdata)
